6cae5567aa5360cc6ebecadcf8646deed742acba
Daniel Ritter <danieldritter1@gmail.com>
2022-03-22 15:49:29 -0500

    Added shell scripts for cora and ppi. Fixed ppi and added batching. Currently ppi micro-f1 score is aroudn 75%, so still need to find a bug there

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import torch.nn.functional as F
 import torch_geometric.transforms as T
 from torch_geometric.datasets import Planetoid, PPI, Amazon
 from torch_geometric.nn import GATConv, GCNConv, GINConv
+from torch_geometric.loader import DataLoader
+from sklearn.metrics import f1_score 
 from GAT import GraphAttentionLayer
 import argparse 
 
@@ -42,7 +44,7 @@ class GATNet(torch.nn.Module):
                 self.conv_layers.append(GraphAttentionLayer(num_hidden_features, num_classes, num_heads[-1], concat=concats[-1], dropout=dropout))
         elif model_name == 'GATGeometric':
             self.conv_layers.append(GATConv(num_features, num_hidden_features, num_heads[0], concat=concats[0], dropout=dropout))
-            for i in range(num_layers-2):
+            for i in range(1,num_layers-1):
                 if concats[i-1]:
                     self.conv_layers.append(GATConv(num_hidden_features*num_heads[i-1], num_hidden_features, num_heads[i], concat=concats[i], dropout=dropout))
                 else:
@@ -86,6 +88,7 @@ def main():
     parser.add_argument("--dataset", default="Cora")
     parser.add_argument("--model", default="GAT")
     parser.add_argument("--learning_rate", type=float, default=.005)
+    parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument("--num_hidden_features", type=int, default=8)
     parser.add_argument("--num_layers", type=int, default=2)
     parser.add_argument("--attention_heads", type=int, nargs="+")
@@ -115,15 +118,15 @@ def main():
         use_planetoid = args.dataset == 'Cora' or args.dataset == 'Citeseer' or args.dataset == 'Pubmed'
 
         if use_planetoid:
-            dataset = Planetoid('./data', args.dataset, split="public", num_train_per_class=20)
+            dataset = Planetoid('./data', args.dataset, split="public", num_train_per_class=20, transform=T.NormalizeFeatures())
             num_features = dataset.num_node_features
             num_classes = dataset.num_classes
         elif args.dataset == 'AmazonComp':
-            dataset = Amazon('./data', 'Computers')
+            dataset = Amazon('./data', 'Computers', transform=T.NormalizeFeatures())
             num_features = 767
             num_classes = 10
         elif args.dataset == 'AmazonPhotos':
-            dataset = Amazon('./data', 'Photo')
+            dataset = Amazon('./data', 'Photo', transform=T.NormalizeFeatures())
             num_features = 745
             num_classes = 8
         elif args.dataset == 'PPI':
@@ -135,17 +138,17 @@ def main():
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
         model = GATNet(num_features, args.num_hidden_features, args.num_layers, num_classes, args.attention_heads, args.concat_layers, args.model, args.dataset, args.dropout_val).to(device)
-        if args.dataset == 'PPI':
-            data = datasetTrain[0].to(device)
-            dataVal = datasetVal[0].to(device)
-            dataTest = datasetTest[0].to(device)
-        else:
-            data = dataset[0].to(device)
-        data = T.NormalizeFeatures()(data)
         optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.l2_lambda)
         if args.verbose:
             print('Starting training...')
-
+        if args.dataset == "PPI":
+            train_loader = DataLoader(datasetTrain, batch_size=args.batch_size, shuffle=True)
+            val_loader = DataLoader(datasetVal, batch_size=args.batch_size)
+            test_loader = DataLoader(datasetTest, batch_size=args.batch_size)
+        else:
+            train_loader = DataLoader(dataset, batch_size=1, shuffle=True)
+            val_loader = train_loader 
+            test_loader = train_loader 
         epoch = 0
         stop_counter = 0
         cur_max = 0.0
@@ -153,39 +156,50 @@ def main():
         stop_training = False
         while not stop_training:
             model.train()
-            optimizer.zero_grad()
-            out = model(data)
-            if args.dataset == 'PPI':
-                pred = (out>0.5).int()
-                loss = F.cross_entropy(out, data.y)
-                correct = (pred == data.y).sum()
-                acc = float(int(correct) / int(data.y.shape[0] * num_classes))
-            else:
-                pred = out.argmax(dim=1)
-                loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-                correct = (pred[data.train_mask] == data.y[data.train_mask]).sum()
-                acc = (correct / data.train_mask.sum()).item()
-            train_losses.append(loss.item())
-            train_accs.append(acc)
-            loss.backward()
-            optimizer.step()
+            for batch in train_loader:
+                batch = batch.to(device)
+                optimizer.zero_grad()
+                out = model(batch)
+                if args.dataset == 'PPI':
+                    pred = (out>0.5).int()
+                    loss = F.binary_cross_entropy_with_logits(out.flatten(), batch.y.flatten())
+                    acc = f1_score(batch.y.cpu().detach().flatten(), pred.cpu().detach().flatten(), average="micro")
+                    # correct = (pred == data.y).sum()
+                    # acc = float(int(correct) / int(data.y.shape[0] * num_classes))
+                else:
+                    pred = out.argmax(dim=1)
+                    loss = F.nll_loss(out[batch.train_mask], batch.y[batch.train_mask])
+                    correct = (pred[batch.train_mask] == batch.y[batch.train_mask]).sum()
+                    acc = (correct / batch.train_mask.sum()).item()
+                train_losses.append(loss.item())
+                train_accs.append(acc)
+                loss.backward()
+                optimizer.step()
             if args.use_early_stopping:
                 if epoch >= args.num_forced_epochs - 1:
                     model.eval()
-                    if args.dataset == 'PPI':
-                        out = model(dataVal)
-                        pred = (out>0.5).int()
-                        loss = F.cross_entropy(out, dataVal.y)
-                        correct = (pred == dataVal.y).sum()
-                        acc = float(int(correct) / int(dataVal.y.shape[0] * num_classes))
-                    else:
-                        out = model(data)
-                        pred = out.argmax(dim=1)
-                        loss = F.nll_loss(out[data.val_mask], data.y[data.val_mask])
-                        correct = (pred[data.val_mask] == data.y[data.val_mask]).sum()
-                        acc = (correct / data.val_mask.sum()).item()
-                    val_losses.append(loss.item())
-                    val_accs.append(acc)
+                    losses = 0.0
+                    accs = 0.0
+                    for batch in val_loader:
+                        batch = batch.to(device)
+                        out = model(batch)
+                        if args.dataset == 'PPI':
+                            pred = (out>0.5).int()
+                            loss = F.binary_cross_entropy_with_logits(out.flatten(), batch.y.flatten())
+                            losses += loss.item()
+                            # correct = (pred == dataVal.y).sum()
+                            # acc = float(int(correct) / int(dataVal.y.shape[0] * num_classes))
+                            acc = f1_score(batch.y.cpu().detach().flatten(), pred.cpu().detach().flatten(), average="micro")
+                            accs += acc 
+                        else:
+                            pred = out.argmax(dim=1)
+                            loss = F.nll_loss(out[batch.val_mask], batch.y[batch.val_mask])
+                            losses += loss.item()
+                            correct = (pred[batch.val_mask] == batch.y[batch.val_mask]).sum()
+                            acc = (correct / batch.val_mask.sum()).item()
+                            accs += acc
+                    val_losses.append(losses/len(val_loader))
+                    val_accs.append(accs/len(val_loader))
                     if acc >= cur_max or loss.item() <= cur_min_loss:
                         if args.verbose:
                             print('Found new validation maximum at epoch ' + str(epoch + 1) + '!')
@@ -214,20 +228,26 @@ def main():
                 if args.verbose:
                     if not epoch == 0 and (epoch + 1) % args.logging_frequency == 0:
                         model.eval()
-                        if args.dataset == 'PPI':
-                            out = model(dataVal)
-                            loss = F.cross_entropy(out, dataVal.y)
-                            pred = (out>0.5).int()
-                            correct = (pred == dataVal.y).sum()
-                            acc = float(int(correct) / int(dataVal.y.shape[0] * num_classes))
-                        else:
-                            out = model(data)
-                            pred = out.argmax(dim=1)
-                            loss = F.nll_loss(out[data.val_mask], data.y[data.val_mask])
-                            correct = (pred[data.val_mask] == data.y[data.val_mask]).sum()
-                            acc = float(int(correct) / int(data.val_mask.sum()))
-                        val_accs.append(acc)
-                        val_losses.append(loss.item())
+                        losses = 0.0 
+                        accs = 0.0 
+                        for batch in val_loader:
+                            batch = batch.to(device)
+                            out = model(batch)
+                            if args.dataset == 'PPI':
+                                loss = F.binary_cross_entropy_with_logits(out.flatten(), batch.y.flatten())
+                                losses += loss.item()
+                                pred = (out>0.5).int()
+                                acc = f1_score(batch.y.cpu().detach().flatten(), pred.cpu().detach().flatten(), average="micro")
+                                accs += acc
+                            else:
+                                pred = out.argmax(dim=1)
+                                loss = F.nll_loss(out[batch.val_mask], batch.y[batch.val_mask])
+                                losses += loss.item()
+                                correct = (pred[batch.val_mask] == batch.y[batch.val_mask]).sum()
+                                acc = float(int(correct) / int(batch.val_mask.sum()))
+                                accs += acc 
+                        val_accs.append(accs/len(val_loader))
+                        val_losses.append(losses/len(val_loader))
                         print('Epoch: ' + str(epoch + 1) + ', Validation Accuracy: ' + str(acc) + '%')
                 if epoch >= args.num_epochs - 1:
                     stop_training = True
@@ -240,17 +260,24 @@ def main():
         if args.use_early_stopping:
             model.load_state_dict(torch.load("./model/cur_model.pt"))
         if args.dataset == 'PPI':
-            pred = (model(dataTest)>0.5).int()
-            correct = (pred == dataTest.y).sum()
-            acc = float(int(correct) / int(dataTest.y.shape[0] * num_classes))
+            accs = 0.0 
+            for batch in test_loader:
+                batch = batch.to(device)
+                pred = (model(batch)>0.5).int()
+                # correct = (pred == dataTest.y).sum()
+                # acc = float(int(correct) / int(dataTest.y.shape[0] * num_classes))
+                acc = f1_score(batch.y.cpu().detach().flatten(), pred.cpu().detach().flatten(), average="micro")
+                accs += acc
         else:
-            pred = model(data).argmax(dim=1)
-            correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
-            acc = float(int(correct) / int(data.test_mask.sum()))
+            for batch in test_loader:
+                batch = batch.to(device)
+                pred = model(batch).argmax(dim=1)
+                correct = (pred[batch.test_mask] == batch.y[batch.test_mask]).sum()
+                acc = float(int(correct) / int(batch.test_mask.sum()))
 
-        print(f'Test Accuracy: {acc:.4f}%')
-        total_avg += acc
-        total_avg_list.append(acc)
+        print(f'Test Accuracy: {accs/len(test_loader):.4f}%')
+        total_avg += accs/len(test_loader)
+        total_avg_list.append(accs/len(test_loader))
 
     print('All Results: ' + str(total_avg_list))
     print(f'Total Test Average: {total_avg/args.num_runs}')
--- run_cora.sh
+++ run_cora.sh
@@ -1,2 +1,2 @@
-python main.py --dataset="PPI" --attention_heads 4 4 6 --num_layers=3 --num_hidden_features=256\
-  --dropout_val=0.0 --concat_layers 1 1 0 --verbose --use_early_stopping --num_runs=10
+python main.py --attention_heads 8 1 --num_hidden_features=8\
+  --dropout_val=0.6 --concat_layers 1 0 --verbose --use_early_stopping --num_runs=10
